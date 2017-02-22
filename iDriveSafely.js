dfb278b5bc50647ead8276d3ba4b05b268f2eec6
Tyler Hoyt <tyler.hoyt@kindlingapps.com>
2017-02-22 00:26:37 -0500

    added logic for module page and fixed pattern for lesson pages

--- iDriveSafely.js
+++ iDriveSafely.js
@@ -1,16 +1,10 @@
-var testPagePattern = "displayTestSequenceQuestions";
-var testPageScript = `
-for (var i=0; i<10; i++) {
-  var q = document.getElementsByTagName("table")[7].getElementsByTagName("tbody")[0].getElementsByTagName("tr")[(i*5)+1].innerText.replace(/^\s*\d*\s*/,"").trim();
-  console.log(q);
-  for(var j=1; j<=4; j++) {
-    var a = document.getElementsByTagName("table")[7].getElementsByTagName("tbody")[0].getElementsByTagName("tr")[(i*5)+1+j].innerText.trim();
-    console.log("    " + a);
-  }
-}
+// TODO: This needs work. each time you click this form submit the current lesson information is passed it. It needs to be parsed.
+var modulePattern = "displayModuleContentTOC";
+var moduleScript = `
+setTimeout(document.getElementsByTagName("a")[document.getElementsByTagName("a").length - 1].click(), 1000);
 `;
 
-var lessonPattern = "thingslkjsdflkjsdflkjsdlfkjsdlfkjsdlfkjsdflkj";
+var lessonPattern = "outputContentHtml";
 var lessonScript = `
 var strTime = "0:00";
 try {
@@ -28,11 +22,25 @@ console.log(total_plus_buffer + " Total Seconds With Buffer");
 setTimeout(function(){formSubmit()}, total_plus_buffer * 1000);
 `;
 
-// TODO: This needs work. each time you click this form submit the current lesson information is passed it. It needs to be parsed.
-var modulePattern = "displayModuleContentTOC";
-var moduleScript = `
-console.log("This module page requires more work")
+var testPagePattern = "displayTestSequenceQuestions";
+var testPageScript = `
+for (var i=0; i<10; i++) {
+  var q = document.getElementsByTagName("table")[7].getElementsByTagName("tbody")[0].getElementsByTagName("tr")[(i*5)+1].innerText.replace(/^\s*\d*\s*/,"").trim();
+  console.log(q);
+  for(var j=1; j<=4; j++) {
+    var a = document.getElementsByTagName("table")[7].getElementsByTagName("tbody")[0].getElementsByTagName("tr")[(i*5)+1+j].innerText.trim();
+    console.log("    " + a);
+    if (a === "") { //TODO: Put in some logic to reach out to my server
+      document.getElementsByTagName("table")[7].getElementsByTagName("tbody")[0].getElementsByTagName("tr")[(i*5)+1+j].getElementsByTagName("td")[0].getElementsByTagName("input")[0].checked = true;
+    }
+  }
+}
 `;
+
+var reviewPagePattern = "displayReviewContentDetails";
+var reviewPageScript = `
+setTimeout(function(){formSubmit()}, 1000);
+`
 
 var script = "";
 
@@ -45,6 +53,9 @@ if(document.URL.includes(testPagePattern)) {
 } else if (document.URL.includes(modulePattern)) {
   console.log("*** This is a module page");
   script = moduleScript;
+} else if (document.URL.includes(reviewPagePattern)) {
+  console.log("*** This is a review page");
+  script = reviewPageScript;
 } else {
   console.log("*** Can't help you");
   alert("Can't help you, sorry!")
